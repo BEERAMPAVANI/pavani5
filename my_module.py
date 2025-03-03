0e694a599c26d801769a4eb86c90046ed0f49c10
BEERAMPAVANI <2311cs020083@mallareddyuniversity.ac.in>
2025-03-03 09:50:17 +0530

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+greet = lambda name : print(f"Good Morning {name}!")
+greet("pavani")
+
+
+# In[2]:
+
+
+product = lambda a ,b ,c :a * b * c
+a=3
+b=6
+c=7
+r = product(a,b,c)
+print(r)
+
+
+# In[5]:
+
+
+even = lambda L : [x for x in L if x%2 ==0]
+a=[100,200,93,18,83,300,9]
+even(a)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
